060201b514aaf5a9158aab0a049043a76ce87693
Henry Hill <hhill1@nd.edu>
2015-12-13 23:30:55 -0500

    professor netid must be unique

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,5 @@ class Professor < ActiveRecord::Base
   has_many :courses
 
   validates :firstname, :lastname, :department, :email, :location, :password, presence: true
+  validates :netid, uniqueness: true
 end
