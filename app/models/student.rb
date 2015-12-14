5d4e505baa3e7fb90fe15faf829fee6a5fa9c3ea
Henry Hill <hhill1@nd.edu>
2015-12-13 21:58:05 -0500

    student netid must be unique

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ActiveRecord::Base
 	has_many :enrollments
 
 	validates :firstname, :lastname, :major, :year, :netid, :email, :password, presence: true
+	validates :netid, uniqueness: true
 end
