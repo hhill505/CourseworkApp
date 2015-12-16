6ac2c9960b70c9d25cc417fe7def2e1cec060032
Henry Hill <hhill1@nd.edu>
2015-12-16 01:53:18 -0500

    Added professor_id to course, added collection-select

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214042327) do
+ActiveRecord::Schema.define(version: 20151216062626) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151214042327) do
     t.text     "description"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "professor_id"
   end
 
   create_table "enrollments", force: :cascade do |t|
